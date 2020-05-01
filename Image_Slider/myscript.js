29166c172392128f7d308bebaa55ceceab4925b0
DeveloperAkram <developeraakram@gmail.com>
2020-05-01 21:20:01 +0600

    Here, Fourth mini project is Image Slider

--- Image_Slider/myscript.js
+++ Image_Slider/myscript.js
@@ -6,15 +6,19 @@ function plusDivs(n) {
 }
 
 function showDivs(n) {
-    
+
     var i;
     var x = document.getElementsByClassName("mySlider");
-    
-    if (n > x.length) { slideIndex = 1}
-    if (n < 1) { slideIndex = x.length}
-    for (i=0; i< x.length; i++) {
+
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    
+
     x[slideIndex - 1].style.display = "block";
 }
